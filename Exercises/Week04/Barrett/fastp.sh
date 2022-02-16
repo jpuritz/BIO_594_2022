1244d79ed551a31695c76115444cfd1a42ae7ea4
gabriel-a-barrett <gabriel.barrett@uconn.edu>
2022-02-16 15:05:22 -0500

    add filtered datasets to Exercises/Week04/Barrett/

---
+++ Exercises/Week04/Barrett/fastp.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+INDIR=/home/BIO594/DATA/Week4
+OUTDIR=/home/gbarrett/BIO_594_2022/Exercises/Week04/Barrett
+#mkdir -p Barrett
+for i in $INDIR/*F.fq.gz;
+do
+	# Input Reads
+	fq1=${i}
+	fq2=$(echo ${i} | sed 's/F./R./')
+    echo $fq1 $fq2
+	# Output Reads
+	FQ1=$(echo ${fq1} | sed -e 's/_1./.out_1./' -e "s+$INDIR+$OUTDIR+")
+	FQ2=$(echo ${fq2} | sed -e 's/_2./.out_2./' -e "s+$INDIR+$OUTDIR+")	
+    echo $FQ1 $FQ2
+	# Output Reports
+	OUTPUT=$(basename $fq1 .F.fq.gz)
+	echo $OUTPUT
+	/usr/local/bin/fastp -i $fq1 -I $fq2 -o $FQ1 -O $FQ2 \
+    --cut_right --cut_mean_quality=25 --cut_window_size 6 \
+	--detect_adapter_for_pe \
+	--correction -h ${OUTDIR}/$OUTPUT.html &> ${OUTDIR}/$OUTPUT.log
+done
